37a3b777018450a9312c2610110a677aeb5fde9b
Jason Crouse <jcrouse@drift.com>
2020-11-19 14:44:50 -0500

    animating the square

--- public/render.js
+++ public/render.js
@@ -1,9 +1,30 @@
-const drawScene = (gl, parameters, buffers) => {
+let squareRotation = 0.0;
+
+const createProjectionMatrix = (gl) => {
+  const fieldOfView = (45 * Math.PI) / 180;
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  const zNear = 0.1;
+  const zFar = 100.0;
+  const projectionMatrix = glMatrix.mat4.create();
+
+  glMatrix.mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+  return projectionMatrix;
+};
+
+const drawScene = (gl, parameters, buffers, deltaTime) => {
   clearScene(gl);
   const projectionMatrix = createProjectionMatrix(gl);
   const modelViewMatrix = glMatrix.mat4.create();
 
   glMatrix.mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
+  glMatrix.mat4.rotate(modelViewMatrix, modelViewMatrix, squareRotation, [
+    0,
+    0,
+    1,
+  ]);
+
+  squareRotation += deltaTime;
+
   configurePositionBufferRead(gl, buffers, parameters);
   configureColorBufferRead(gl, buffers, parameters);
 
@@ -22,17 +43,6 @@ const clearScene = (gl) => {
   gl.enable(gl.DEPTH_TEST);
   gl.depthFunc(gl.LEQUAL);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-};
-
-const createProjectionMatrix = (gl) => {
-  const fieldOfView = (45 * Math.PI) / 180;
-  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-  const zNear = 0.1;
-  const zFar = 100.0;
-  const projectionMatrix = glMatrix.mat4.create();
-
-  glMatrix.mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
-  return projectionMatrix;
 };
 
 const configurePositionBufferRead = (gl, buffers, parameters) => {
